5300d4679d618353995aafd9afc4d2699bb56c01
ravian606 <bakar222@gmail.com>
2022-07-19 16:41:07 +0500

    Fixed contacts adding issue

--- db/migrate/20220719113204_add_company_id_foreign_key_contact.rb
+++ db/migrate/20220719113204_add_company_id_foreign_key_contact.rb
@@ -1,5 +1,5 @@
 class AddCompanyIdForeignKeyContact < ActiveRecord::Migration[6.0]
   def change
-    add_reference :contacts, :company, foreign_key: true
+    #add_reference :contacts, :company, foreign_key: true
   end
 end
