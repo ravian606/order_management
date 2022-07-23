f52b0e85c43ae6111ba2943745343cd8a01238a4
ravian606 <bakar222@gmail.com>
2022-07-23 16:32:16 +0500

    Fixed migrations

--- db/migrate/20220711123646_add_foreign_keys.rb
+++ db/migrate/20220711123646_add_foreign_keys.rb
@@ -1,6 +1,6 @@
 class AddForeignKeys < ActiveRecord::Migration[6.0]
   def change
-    add_reference :orders, :product, foreign_key: true
-    add_reference :sites, :company, foreign_key: true
+    #add_reference :orders, :product, foreign_key: true
+    #add_reference :sites, :company, foreign_key: true
   end
 end
